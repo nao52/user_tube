7a70e3d7217a004b9fa53285f11b1432bda71213
nao52 <naoki.52abc@icloud.com>
2023-11-24 15:22:52 +0900

    rspecの設定 / rubocopの設定 / generatorの設定

--- Gemfile
+++ Gemfile
@@ -62,11 +62,15 @@ group :development do
 
   # Speed up commands on slow machines / big apps [https://github.com/rails/spring]
   # gem "spring"
+
+  gem "rubocop", require: false
+  gem "rubocop-rails", require: false
 end
 
 group :test do
   # Use system testing [https://guides.rubyonrails.org/testing.html#system-testing]
   gem "capybara"
   gem "selenium-webdriver"
-
+  gem "rspec-rails"
+  gem "factory_bot_rails"
 end
--- config/application.rb
+++ config/application.rb
@@ -11,12 +11,10 @@ module UserTube
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    config.generators do |g|
+      g.skip_routes true
+      g.helper false
+      g.test_framework :rspec
+    end
   end
 end
