f7bcccc2c7224ed4b51ff92a69f937c127c43785
nao52 <naoki.52abc@icloud.com>
2023-11-28 15:55:46 +0900

    登録画面の作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   root "static_pages#top"
+  get  "/signup", to: "users#new"
+  post "/singup/confirm", to: "users#confirm"
+
+  resources :users, only: %i[create]
 end
