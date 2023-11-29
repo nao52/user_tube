1e4a3971ff1aec9dd87f22674162db84d2617924
nao52 <naoki.52abc@icloud.com>
2023-11-29 14:24:21 +0900

    ログイン画面の実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,12 @@ Rails.application.routes.draw do
   root "static_pages#top"
   get  "/signup", to: "users#new"
   post "/singup/confirm", to: "users#signup_confirm"
+  get "login", to: "user_sessions#new"
 
   resources :users, only: %i[create] do
     collection do
       get :confirm
     end
   end
+  resources :user_sessions, only: %i[create destroy]
 end
