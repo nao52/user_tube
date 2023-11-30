769916351aefc9a0d4c95c25fd0bfdf3a33118b8
nao52 <naoki.52abc@icloud.com>
2023-11-30 14:17:33 +0900

    ユーザーデータの準備

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# 初期ユーザー
+User.create(
+  name: "ナゲット",
+  email: "nage@test.com",
+  password: "nagenage",
+  password_confirmation: "nagenage",
+  age: 18,
+  gender: 1
+)
+
+50.times do |n|
+  User.create(
+    name: Faker::Name.name,
+    email: "test#{n+1}@example.com",
+    password: "password",
+    password_confirmation: "password"
+  )  
+end
