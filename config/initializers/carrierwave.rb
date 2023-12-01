b870bd4250cc6ecb38bcf622d1dfe717c36f7d47
nao52 <naoki.52abc@icloud.com>
2023-12-01 17:11:51 +0900

    carrierwaveの設定を変更

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,16 +1,21 @@
 require 'carrierwave/storage/abstract'
+require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
-  config.storage :fog
-  config.fog_provider = 'fog/aws'
-  config.fog_directory = 'user-tube-bucket'
-  config.fog_public = true
-  config.fog_credentials = {
-    provider: 'AWS',
-    aws_access_key_id: Rails.application.credentials.aws[:access_key_id],
-    aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
-    region: 'ap-northeast-1',
-    path_style: true
-  }
+  if Rails.env.production?
+    config.storage :fog
+    config.fog_provider = 'fog/aws'
+    config.fog_directory = 'user-tube-bucket'
+    config.fog_public = false
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: ENV['AWS_ACCESS_KEY'],
+      aws_secret_access_key: ENV['AWS_SECRET_KEY'],
+      region: 'ap-northeast-1',
+      path_style: true
+    }
+  else
+    config.storage :file
+  end
 end
