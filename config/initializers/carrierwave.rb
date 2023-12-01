026072d7d398980b15281b8e868f58aa45ba8df8
nao52 <naoki.52abc@icloud.com>
2023-12-01 16:41:51 +0900

    本番環境用のcarrierwaveの設定を変更

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,14 +1,16 @@
+require 'carrierwave/storage/abstract'
+require 'carrierwave/storage/fog'
+
 CarrierWave.configure do |config|
+  config.storage :fog
+  config.fog_provider = 'fog/aws'
+  config.fog_directory = 'user-tube-bucket'
+  config.fog_public = true
   config.fog_credentials = {
-    provider:              'AWS',
-    aws_access_key_id:     Rails.application.credentials.aws[:access_key_id],
+    provider: 'AWS',
+    aws_access_key_id: Rails.application.credentials.aws[:access_key_id],
     aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
-    # use_iam_profile:       true,
-    region:                'ap-northeast-1',
-    host:                  's3.example.com',
-    endpoint:              'https://s3.example.com:8080'
+    region: 'ap-northeast-1',
+    path_style: true
   }
-  config.fog_directory  = 'user-tube-bucket'
-  config.fog_public     = false
-  config.cache_storage = :file
 end
