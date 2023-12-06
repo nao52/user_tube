3c5a44779aec3fd50e042b207663a681638268ac
nao52 <naoki.52abc@icloud.com>
2023-12-06 10:31:05 +0900

    フォロー機能のテストを実装

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -4,18 +4,12 @@ class RelationshipsController < ApplicationController
   def create
     @followed_user = User.find(params[:followed_id])
     current_user.follow(@followed_user)
-    respond_to do |format|
-      # format.html { redirect_to users_url }
-      format.turbo_stream
-    end
+    respond_to(&:turbo_stream)
   end
 
   def destroy
     @followed_user = User.find(params[:id])
     current_user.unfollow(@followed_user)
-    respond_to do |format|
-      # format.html { redirect_to users_url, status: :see_other }
-      format.turbo_stream
-    end
+    respond_to(&:turbo_stream)
   end
 end
