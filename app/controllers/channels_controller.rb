eca8b3049ceda1d072e3c10db31d1dcf91a0ce17
nao52 <naoki.52abc@icloud.com>
2023-12-13 15:42:46 +0900

    チャンネル詳細画面(高評価動画一覧、ユーザー一覧)を実装

--- app/controllers/channels_controller.rb
+++ app/controllers/channels_controller.rb
@@ -4,7 +4,10 @@ class ChannelsController < ApplicationController
   end
 
   def show
+    @link = params[:link] ||= 'comment'
     @channel = Channel.find(params[:id])
-    @channel_comments = @channel.channel_comments.includes(:user).page(params[:page])
+    @channel_comments = @channel.channel_comments.includes(:user).page(params[:page]) if @link == 'comment'
+    @channel_videos = @channel.videos.page(params[:page]) if @link == 'video'
+    @channel_users = @channel.users.page(params[:page]) if @link == 'user'
   end
 end
