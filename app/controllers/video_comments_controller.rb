6a0b8aee7f84c042ef244a51c33d909dbb1ec45f
nao52 <naoki.52abc@icloud.com>
2023-12-14 14:56:07 +0900

    高評価動画に対するコメントの作成ページおよび編集ページの実装

--- app/controllers/video_comments_controller.rb
+++ app/controllers/video_comments_controller.rb
@@ -1,5 +1,37 @@
 class VideoCommentsController < ApplicationController
-  def new; end
+  before_action :require_login
+  before_action :set_video, only: %i[new create]
+  before_action :set_video_comment, only: %i[edit update destroy]
+
+  def new
+    @video_comment = current_user.video_comments.build
+  end
+
+  def create
+    raise
+  end
 
   def edit; end
+
+  def update
+    raise
+  end
+
+  def destroy
+    raise
+  end
+
+  private
+
+  def video_comment_params
+    params.require(:video_comment).permit(:body, :video_id)
+  end
+
+  def set_video
+    @video = Video.find(params[:video_id])
+  end
+
+  def set_video_comment
+    @video_comment = current_user.video_comments.find(params[:id])
+  end
 end
