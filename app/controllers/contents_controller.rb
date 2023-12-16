380f0ee5ed58f49da77da9c99f6babff1ae69efa
nao52 <naoki.52abc@icloud.com>
2023-12-16 20:50:52 +0900

    投稿詳細ページの実装

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -2,4 +2,8 @@ class ContentsController < ApplicationController
   def index
     @contents = Content.all.page(params[:page]).per(8)
   end
+
+  def show
+    @content = Content.find(params[:id])
+  end
 end
