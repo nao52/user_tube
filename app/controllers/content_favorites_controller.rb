a3ae69dc862124d816a4918d078d77e4d675305f
nao52 <naoki.52abc@icloud.com>
2023-12-19 16:05:46 +0900

    いいね機能のAjax化

--- app/controllers/content_favorites_controller.rb
+++ app/controllers/content_favorites_controller.rb
@@ -1,15 +1,21 @@
 class ContentFavoritesController < ApplicationController
-  before_action :require_login, only: %i[edit update]
+  before_action :require_login, only: %i[create destroy]
 
   def create
     @content = Content.find(params[:content_id])
     current_user.like_content(@content)
-    redirect_to contents_url, success: t('.success')
+    respond_to do |format|
+      format.html { redirect_to contents_url, success: t('.success') }
+      format.turbo_stream { flash.now[:success] = t('.success') }
+    end
   end
 
   def destroy
     @content = Content.find(params[:id])
     current_user.dislike_content(@content)
-    redirect_to contents_url, success: t('.success')
+    respond_to do |format|
+      format.html { redirect_to contents_url, success: t('.success'), status: :see_other }
+      format.turbo_stream { flash.now[:success] = t('.success') }
+    end
   end
 end
