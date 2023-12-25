6412fd6ad5317aea3501de54c18b8b0691b187ad
nao52 <naoki.52abc@icloud.com>
2023-12-25 19:26:18 +0900

    本番環境でテストデータを作成(リリース後に設定を戻す)

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -4,4 +4,10 @@ set -o errexit
 bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
-bundle exec rails db:migrate
+
+# アプリリリース後に戻す
+# bundle exec rails db:migrate
+
+# 本番環境にテストデータを作成
+DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rails db:migrate:reset
+bundle exec rails db:seed
