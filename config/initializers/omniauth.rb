ed99f5a37a077eb85dd5b4a5a05b9331987a057c
nao52 <naoki.52abc@icloud.com>
2024-01-05 16:36:53 +0900

    Youtube Data Apiのscopeを変更

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, Settings.google_client_id, Settings.google_client_secret, scope: 'email, profile, https://www.googleapis.com/auth/youtube.force-ssl'
+  provider :google_oauth2, Settings.google_client_id, Settings.google_client_secret, scope: 'email https://www.googleapis.com/auth/youtube.readonly'
 end
