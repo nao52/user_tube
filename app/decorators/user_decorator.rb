27bd765e4a96a715c0e2280526e7b555fb8d0295
nao52 <naoki.52abc@icloud.com>
2024-01-12 15:40:28 +0900

    年齢を年代で表示されるように変更

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -4,4 +4,10 @@ class UserDecorator < ApplicationDecorator
   def interesting_category
     categories.present? ? categories.map(&:title_i18n).join(' / ') : '設定なし'
   end
+
+  def generation
+    return '非公開' if age.nil?
+
+    age < 60 ? "#{age.floor(-1)}代" : '60以上'
+  end
 end
