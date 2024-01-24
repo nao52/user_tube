60325bb81591642cd4cb39e492bfb1d6bf6bbb87
nao52 <naoki.52abc@icloud.com>
2024-01-24 22:03:58 +0900

    高評価された動画のカテゴリーからチャンネルのカテゴリーを決定

--- app/decorators/channel_decorator.rb
+++ app/decorators/channel_decorator.rb
@@ -1,12 +1,7 @@
 class ChannelDecorator < ApplicationDecorator
   delegate_all
 
-  # Define presentation-specific methods here. Helpers are accessed through
-  # `helpers` (aka `h`). You can override attributes, for example:
-  #
-  #   def created_at
-  #     helpers.content_tag :span, class: 'time' do
-  #       object.created_at.strftime("%a %m/%d/%y")
-  #     end
-  #   end
+  def categories
+    videos.map { |video| video.category.title_i18n }.uniq.join(' / ')
+  end
 end
