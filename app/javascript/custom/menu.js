9a38c7630cd50c31483e7e9841c6fb173cc40485
nao52 <naoki.52abc@icloud.com>
2024-01-31 13:34:00 +0900

    ヘッダーをレスポンシブデザインに対応

--- app/javascript/custom/menu.js
+++ app/javascript/custom/menu.js
@@ -1,16 +1,32 @@
 document.addEventListener("turbo:load", function() {
   let account_name = document.querySelector("#account-name");
-  if (account_name === null) {
-    return
+  let hamburger = document.querySelector("#hamburger");
+
+  if (account_name !== null) {
+    account_name.addEventListener("click", function(event) {
+      event.preventDefault();      
+      let menu = document.querySelector("#dropdown-menu");
+      menu.classList.toggle("active");
+      document.addEventListener("click", function(event) {
+        if(event.target.closest('#dropdown') === null) {
+          menu.classList.remove("active");
+        }
+      });
+    });
   }
-  account_name.addEventListener("click", function(event) {
-    event.preventDefault();
-    let menu = document.querySelector("#dropdown-menu");
-    menu.classList.toggle("active");
-    document.addEventListener("click", function(event) {
-      if(event.target.closest('#dropdown') === null) {
-        menu.classList.remove("active");
+
+  if (hamburger !== null) {
+    hamburger.addEventListener("click", function(event) {
+      event.preventDefault();
+
+      const navbarMenu = document.querySelector("#navbar-menu");
+      const is_menu_visible = navbarMenu.classList.contains("in");
+
+      if (is_menu_visible) {
+        navbarMenu.classList.remove("in");
+      } else {
+        navbarMenu.classList.add("in");
       }
     });
-  });
+  }
 });
