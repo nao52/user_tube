a062085206f108f185bdf371e5c05f8597fc2099
nao52 <naoki.52abc@icloud.com>
2024-02-06 16:25:15 +0900

    チャンネル詳細ページに、動画一覧のページを実装

--- app/controllers/channels_controller.rb
+++ app/controllers/channels_controller.rb
@@ -7,12 +7,14 @@ class ChannelsController < ApplicationController
   end
 
   def show
-    @link = params[:link] ||= 'comment'
+    @link = params[:link] ||= 'user'
     @channel = Channel.find(params[:id])
-    @channel_comments = @channel.channel_comments.includes(:user).page(params[:page]) if @link == 'comment'
-    @channel_videos = @channel.videos.page(params[:page]) if @link == 'video'
     @channel_users = @channel.users_with_public.page(params[:page]) if @link == 'user'
+    @channel_videos = @channel.videos.page(params[:page]).per(8) if @link == 'favorite_video'
+    @channel_comments = @channel.channel_comments.includes(:user).page(params[:page]) if @link == 'comment'
   end
+
+  
 
   private
 
