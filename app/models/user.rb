046e3fa6c521f4623e81299db860b275fcc1bdf2
nao52 <naoki.52abc@icloud.com>
2024-02-11 19:56:42 +0900

    ユーザー削除の際に関連する投稿も削除されるように変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
 
   before_save :downcase_email
 
-  has_many :contents
+  has_many :contents, dependent: :destroy
   has_many :active_relationships,  class_name: 'Relationship', foreign_key: 'follower_id', dependent: :destroy
   has_many :passive_relationships, class_name: 'Relationship', foreign_key: 'followed_id', dependent: :destroy
   has_many :following, through: :active_relationships,  source: :followed
