3c7f1a01fa00173f16d4714db1ea3cdd4381fd0f
nao52 <naoki.52abc@icloud.com>
2024-02-26 12:27:01 +0900

    チャンネルプレイリストモデルのテスト

--- spec/models/channel_playlist_spec.rb
+++ spec/models/channel_playlist_spec.rb
@@ -8,17 +8,29 @@ RSpec.describe ChannelPlaylist, type: :model do
       expect(playlist.errors).to be_empty
     end
 
-    it "playlist_idがない場合にバリデーションが機能してinvalidになるか" do
+    it "プレイリストIDがない場合にバリデーションが機能してinvalidになるか" do
       playlist_without_playlist_id = build(:channel_playlist, playlist_id: "")
       expect(playlist_without_playlist_id).to be_invalid
       expect(playlist_without_playlist_id.errors).to_not be_empty
     end
 
-    it "playlist_idがすでに登録されている場合にvalidationが機能してinvalidになるか" do
+    it "プレイリストIDがすでに登録されている場合にvalidationが機能してinvalidになるか" do
       playlist = create(:channel_playlist)
       duplicate_playlist = playlist.dup
       expect(duplicate_playlist).to be_invalid
       expect(duplicate_playlist.errors).to_not be_empty
+    end
+
+    it "チャンネルIDがない場合にバリデーションが機能してinvalidになるか" do
+      playlist_without_channel_id = build(:channel_playlist, channel: nil)
+      expect(playlist_without_channel_id).to be_invalid
+      expect(playlist_without_channel_id.errors).to_not be_empty
+    end
+
+    it "タイトルがない場合にバリデーションが機能してinvalidになるか" do
+      playlist_without_title = build(:channel_playlist, title: nil)
+      expect(playlist_without_title).to be_invalid
+      expect(playlist_without_title.errors).to_not be_empty
     end
   end
 end
