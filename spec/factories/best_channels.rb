3a537a02a3d0a0dd4c2635dc7d188a121276939a
nao52 <naoki.52abc@icloud.com>
2024-03-01 11:15:32 +0900

    BestChannelモデルのテスト

--- spec/factories/best_channels.rb
+++ spec/factories/best_channels.rb
@@ -2,5 +2,18 @@ FactoryBot.define do
   factory :best_channel do
     association :user
     association :channel
+    rank { 1 }
+
+    factory :best_channel_rank_1 do
+      rank { 1 }
+    end
+
+    factory :best_channel_rank_2 do
+      rank { 2 }
+    end
+
+    factory :best_channel_rank_3 do
+      rank { 3 }
+    end
   end
 end
