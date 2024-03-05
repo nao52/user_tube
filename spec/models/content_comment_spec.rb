9f27d4fcc6c259c8d5145dc4cfc4eebd3a0d13cd
nao52 <naoki.52abc@icloud.com>
2024-03-05 09:53:15 +0900

    ContentCommentモデルのテスト

--- spec/models/content_comment_spec.rb
+++ spec/models/content_comment_spec.rb
@@ -8,25 +8,25 @@ RSpec.describe ContentComment, type: :model do
       expect(content_comment.errors).to be_empty
     end
 
-    it 'bodyがない場合にバリデーションが機能してinvalidになるか' do
+    it 'コメント内容がない場合にバリデーションが機能してinvalidになるか' do
       content_comment_without_body = build(:content_comment, body: '')
       expect(content_comment_without_body).to be_invalid
       expect(content_comment_without_body.errors.full_messages).to include "コメントを入力してください"
     end
 
-    it 'bodyが401文字以上の場合にvalidationが機能してinvalidになるか' do
+    it 'コメント内容が401文字以上の場合にvalidationが機能してinvalidになるか' do
       content_comment_with_long_body = build(:content_comment, body: "a" * 401)
       expect(content_comment_with_long_body).to be_invalid
       expect(content_comment_with_long_body.errors.full_messages).to include "コメントは400文字以内で入力してください"
     end
 
-    it 'user_idがない場合にバリデーションが機能してinvalidになるか' do
+    it 'ユーザーIDがない場合にバリデーションが機能してinvalidになるか' do
       content_comment_without_user_id = build(:content_comment, user: nil)
       expect(content_comment_without_user_id).to be_invalid
       expect(content_comment_without_user_id.errors).to_not be_empty
     end
 
-    it 'content_idがない場合にバリデーションが機能してinvalidになるか' do
+    it 'コンテンツIDがない場合にバリデーションが機能してinvalidになるか' do
       content_comment_without_content_id = build(:content_comment, content: nil)
       expect(content_comment_without_content_id).to be_invalid
       expect(content_comment_without_content_id.errors).to_not be_empty
